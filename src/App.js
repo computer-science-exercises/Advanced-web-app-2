e64c0644011e85df9552fb51e7ff93dc868e7574
avrahamSilberg <avisilberg@gmail.com>
2022-12-11 21:20:39 +0200

    add useEffects and load data from DB

--- src/App.js
+++ src/App.js
@@ -1,72 +1,41 @@
 import './App.css';
-import Milk from "./assets/milk.jpeg";
-import SecondMilk from "./assets/secondMilk.jpeg";
-import Flour from "./assets/flour.jpeg";
-import SecondFlour from "./assets/secondFlour.jpeg";
-import Oil from "./assets/oil.jpeg";
-import SecondOil from "./assets/secondOil.jpeg";
-import Eggs from "./assets/eggs.jpeg";
-import SecondEggs from "./assets/secondEggs.jpeg";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Route, BrowserRouter} from "react-router-dom";
 import Details from "./components/details/details";
+import Profile from "./components/profile/profile";
 import {Routes} from "react-router";
-import CartItems from "./components/cart/cart-items/CartItems";
 import Cart from "./components/cart/Cart";
 import GroceryStore from "./components/grocery-store/GroceryStore";
+import HttpService from "./services/httpService";
 
 function App() {
+    const [cartGroceries, setCartGroceries] = useState([]);
+    const [storeGroceries, setStoreGroceries] = useState([]);
 
-    const  [cartGroceries, setCartGroceries] = useState( []);
+    useEffect(() => {
+            HttpService.getAllStoreProducts().then(res => {
+                    setStoreGroceries(res.data)
+                }
+            )
 
-    const storeGroceries = [
-        {
-            id: 1,
-            name: 'Milk',
-            img: Milk,
-            price: 6.23,
-            store: 'Shufersal',
-            secondImg: SecondMilk,
-            description: 'milk milk milk',
-        },
-        {
-            id: 2,
-            name: 'Flour',
-            img: Flour,
-            price: 4.50,
-            store: 'Rami Levi',
-            secondImg: SecondFlour,
-            description: 'flour flour flour',
-        },
-        {
-            id: 3,
-            name: 'Oil',
-            img: Oil,
-            price: 15.90,
-            store: 'Shufersal',
-            secondImg: SecondOil,
-            description: 'oil oil oil',
-        },
-        {
-            id: 4,
-            name: 'Eggs',
-            img: Eggs,
-            price: 22.40,
-            store: 'Mega',
-            secondImg: SecondEggs,
-            description: 'eggs eggs eggs',
-        },
-    ];
+            HttpService.getAllCartProducts().then(r => {
+                    setCartGroceries(r.data.products)
+                }
+            )
+        }, [false]
+    );
 
     return (
 
         <div className="App">
             <BrowserRouter>
                 <Routes>
-                    <Route path="/cart" element={<Cart groceries={cartGroceries} setCartGroceries={setCartGroceries}/>}/>
-                    <Route path="/grocery-store" element={<GroceryStore groceries={storeGroceries} cartGroceries={cartGroceries}  setCartGroceries={setCartGroceries}/>}/>
-                    <Route path="/" element={<GroceryStore groceries={storeGroceries} cartGroceries={cartGroceries} setCartGroceries={setCartGroceries}/>}/>
+                    <Route path="/cart"
+                           element={<Cart groceries={cartGroceries} setCartGroceries={setCartGroceries}/>}/>
+                    <Route path="/" element={<GroceryStore groceries={storeGroceries} cartGroceries={cartGroceries}
+                                                           setCartGroceries={setCartGroceries}/>}/>
                     <Route path="details/:id" element={<Details groceries={storeGroceries}/>}/>
+                    <Route path="/profile" element={<Profile/>}/>
                 </Routes>
             </BrowserRouter>
 
